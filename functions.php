bb37191f28fc3f05650242e88e0b7fe911f958cd
k24tien <kt7.2017.bl@gmail.com>
2017-12-22 16:51:07 +0700

    first commit

--- functions.php
+++ functions.php
@@ -26,11 +26,57 @@ function checkLogin($conn,$user,$pwd){
     //print_r($fQuery);
     
     if($queryDbFind){
+		$_SESSION['name'] = $queryDbFind["name"];
+         //echo  $_SESSION['name'] ;
         header('Location: newpost.php');
     }
     else
         echo "Wrong user or password!";
 }
-
-
+function showCategory($conn){
+    $cursor = $conn->find();
+    $count = 0;
+    foreach ($cursor as $document) {
+        $count = $count + 1;
+        echo "<tr>";
+        echo "<td>".$count."</td>";
+        foreach($document as $key => $val) {
+            if($key != "_id"){
+            echo "<td>".$val."</td>";
+            }
+        }
+        echo "<td></td>";
+        echo "</tr>";
+    }
+}
+function showCategoryinSelectbox($conn){
+    $cursor = $conn->find();
+    $count = 0;
+    echo '<select name="category" class="form-control">';
+    foreach ($cursor as $document) {
+        
+        //echo '<option value="'.$document['_id'].'">'.$document['catname'].'</option>';
+        echo '<option value="'.$document['catname'].'">'.$document['catname'].'</option>';
+    }
+    echo '</select>';
+}
+function showAllPost($conn){
+    $cursor = $conn->find();
+    $count = 0;
+    foreach ($cursor as $document) {
+        $count = $count + 1;
+        echo "<tr>";
+        echo "<td>".$count."</td>";
+        echo "<td>".$document['title']."</td>";
+        echo "<td>".$document['author']."</td>";
+        echo "<td>".date('d/M/Y', $document['created_date']->sec)."</td>";
+        echo "<td>".$document['category']."</td>";
+        echo "<td>".$document['place']."</td>";
+        echo "<td>10</td>";
+        echo "<td>10</td>";
+        echo '<td class="text-centered"><i class="fa fa-pencil-square-o"></i></td>';
+        echo '<td class="text-centered"><i class="fa fa-times"></i></td></td>';
+        echo "</tr>";
+    }
+}
 ?>
