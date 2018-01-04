0cf94bad42d27429ccdb6957872d61151d5703ae
k24tien <kt7.2017.bl@gmail.com>
2018-01-04 16:08:32 +0700

    first commit

--- left-sidebar.php
+++ left-sidebar.php
@@ -1,24 +1,24 @@
 <div class="col-lg-3">
 	<aside class="right-sidebar">
 		<div class="widget">
-			<h5 class="widgetheading">Bài viết</h5>
+			<h5 class="widgetheading">Quản lý bài viết</h5>
 			<ul class="cat">
-				<li><i class="fa fa-angle-right"></i><a href="newpost.php">Bài viết mới</a><span> (..)</span></li>
-				<li><i class="fa fa-angle-right"></i><a href="allpost.php">Tất cả bài viết</a><span> (..)</span></li>
+				<li><i class="fa fa-angle-right"></i><a href="newpost.php">Thêm mới</a></li>
+				<li><i class="fa fa-angle-right"></i><a href="allpost.php">Danh sách bài viết</a><span> (..)</span></li>
 			</ul>
 		</div>
 		<div class="widget">
-			<h5 class="widgetheading">Chuyên mục</h5>
+			<h5 class="widgetheading">Quản lý chuyên mục</h5>
 			<ul class="cat">
-				<li><i class="fa fa-angle-right"></i><a href="newcategory.php">Chuyên mục mới</a><span> (..)</span></li>
-				<li><i class="fa fa-angle-right"></i><a href="allcategory.php">Tất cả chuyên mục</a><span> (..)</span></li>
+				<li><i class="fa fa-angle-right"></i><a href="newcategory.php">Thêm mới</a></li>
+				<li><i class="fa fa-angle-right"></i><a href="allcategory.php">Danh sách chuyên mục</a><span> (..)</span></li>
 			</ul>
 		</div>
         <div class="widget">
-			<h5 class="widgetheading">Địa điểm</h5>
+			<h5 class="widgetheading">Quản lý địa điểm</h5>
 			<ul class="cat">
-				<li><i class="fa fa-angle-right"></i><a href="newplace.php">Địa điểm mới</a><span> (..)</span></li>
-				<li><i class="fa fa-angle-right"></i><a href="allplace.php">Tất cả Địa điểm</a><span> (..)</span></li>
+				<li><i class="fa fa-angle-right"></i><a href="newplace.php">Thêm mới</a></li>
+				<li><i class="fa fa-angle-right"></i><a href="allplace.php">Danh sách địa điểm</a><span> (..)</span></li>
 			</ul>
 		</div>
 		
